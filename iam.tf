ed458139d56886a6642189885c2d84bc32b38ea8
Samiwel Thomas <samiwel.thomas@gmail.com>
2021-05-10 11:38:51 +0100

    Initial commit.

---
+++ iam.tf
@@ -0,0 +1,41 @@
+resource "google_project_iam_binding" "veeam-default-binding" {
+  role = google_project_iam_custom_role.veeam-default.id
+  members = [
+    "serviceAccount:${google_service_account.veeam-backup.email}"
+  ]
+}
+
+resource "google_project_iam_binding" "veeam-backup-binding" {
+  role = google_project_iam_custom_role.veeam-backup.id
+  members = [
+    "serviceAccount:${google_service_account.veeam-backup.email}"
+  ]
+}
+
+resource "google_project_iam_binding" "veeam-snapshot-binding" {
+  role = google_project_iam_custom_role.veeam-snapshot.id
+  members = [
+    "serviceAccount:${google_service_account.veeam-backup.email}"
+  ]
+}
+
+resource "google_project_iam_binding" "veeam-repository-binding" {
+  role = google_project_iam_custom_role.veeam-repository.id
+  members = [
+    "serviceAccount:${google_service_account.veeam-backup.email}"
+  ]
+}
+
+resource "google_project_iam_binding" "veeam-restore-binding" {
+  role = google_project_iam_custom_role.veeam-restore.id
+  members = [
+    "serviceAccount:${google_service_account.veeam-backup.email}"
+  ]
+}
+
+resource "google_project_iam_binding" "veeam-worker-binding" {
+  role = google_project_iam_custom_role.veeam-worker.id
+  members = [
+    "serviceAccount:${google_service_account.veeam-backup.email}"
+  ]
+}
